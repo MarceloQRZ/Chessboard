7dc089e0eeaec697a89dbbf67e68af3cb44b9261
Marcelo Albuquerque <marcelo03@alu.ufc.br>
2023-10-24 16:30:09 -0300

    handling errors with custom exceptions

--- Chess_Console/Program.cs
+++ Chess_Console/Program.cs
@@ -8,14 +8,21 @@ namespace Chess_Console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(Cor.Azul, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(Cor.Azul, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Azul, tab), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(Cor.Azul, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(Cor.Azul, tab), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(Cor.Azul, tab), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab); 
-            Console.ReadLine(); 
+                Tela.imprimirTabuleiro(tab);
+                Console.ReadLine();
+            }
+            catch(TabuleiroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+             
         }
     }
 }
